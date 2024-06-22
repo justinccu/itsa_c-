5f00c6da66e8ff927e7917032e4d6dc9b84a798b
justinccu <90822649+justinccu@users.noreply.github.com>
2024-06-22 21:42:47 +0800

    Add files via upload

---
+++ 打招呼程式.cpp
@@ -0,0 +1,22 @@
+/*
+
+問題描述：
+請寫一個打招呼程式。只要輸入姓名，就會跟你問好。
+輸入說明：
+輸入姓名。
+輸出說明：
+輸出打招呼。
+
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << "Hello " << s << endl;
+    return 0;
+}
